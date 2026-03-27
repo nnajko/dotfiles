5111952df3983d4425065f5e784217f494f8855e
Niklas Blomqvist <niklas.b@vagabond.com>
2026-03-27 13:21:07 +0100

    Add yazi.nvim as file explorer, replace netrw

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -1,5 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "File explorer" })
-
 -- Move selected lines up/down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
---
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,11 @@
+return {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+        { "<leader>pv", "<cmd>Yazi<cr>", desc = "Open yazi at current file" },
+        { "<leader>pw", "<cmd>Yazi cwd<cr>", desc = "Open yazi in working directory" },
+    },
+    opts = {
+        open_for_directories = true,
+    },
+}
